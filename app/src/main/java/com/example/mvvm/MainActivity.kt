56edf906aede4bae34e7ad8cbb3bd007a5fd3b4c
Yaroslav Zavyalov <yaroslavzavyalov1@gmail.com>
2024-04-21 12:23:29 +0300

    Добавил фрагменты экранов и логику добавления/удаления/редактирования

--- app/src/main/java/com/example/mvvm/MainActivity.kt
+++ app/src/main/java/com/example/mvvm/MainActivity.kt
@@ -2,20 +2,27 @@ package com.example.mvvm
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.mvvm.ui.theme.MVVMTheme
+import androidx.lifecycle.ViewModelProvider
+import com.example.mvvm.database.NoteDatabase
+import com.example.mvvm.repositories.NoteRepository
+import com.example.mvvm.viewmodels.NoteViewModel
+import com.example.mvvm.viewmodels.NoteViewModelFactory
 
 class MainActivity : ComponentActivity() {
+
+    lateinit var noteViewModel: NoteViewModel
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        setupViewModel()
+    }
+
+    private fun setupViewModel() {
+        val noteDatabase = NoteDatabase(this)
+        val noteRepository = NoteRepository(noteDatabase)
+        val viewModelProviderFactory = NoteViewModelFactory(application, noteRepository)
+        noteViewModel = ViewModelProvider(this, viewModelProviderFactory)[NoteViewModel::class.java]
     }
 }
